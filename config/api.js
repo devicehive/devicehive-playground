bc606de96f53bba67c9147191fbdfe344cd1ba40
Sergey Demyanov <s.demyanov@gmail.com>
2015-09-16 10:55:43 -0400

    Enabling configuration via environment variables

--- config/api.js
+++ config/api.js
@@ -1,10 +1,10 @@
 ﻿module.exports = {
   
-  api_url: "http://nn8576.pg.devicehive.com/api",
-  admin_url: "http://nn8576.pg.devicehive.com/admin",
-  swagger_url: "http://nn8576.pg.devicehive.com/api/swagger",
+  api_url: process.env.DH_API_URL || "http://nn8576.pg.devicehive.com/api",
+  admin_url: process.env.DH_ADMIN_URL || "http://nn8576.pg.devicehive.com/admin",
+  swagger_url: process.env.DH_SWAGGER_URL || "http://nn8576.pg.devicehive.com/api/swagger",
     
-  admin_username: 'admin',
-  admin_password: 'dhadmin_#911'
+  admin_username: process.env.DH_USER || 'admin',
+  admin_password: process.env.DH_PASS || 'dhadmin_#911'
 
 };
