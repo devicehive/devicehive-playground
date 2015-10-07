4162853436e6d91acb1d701cfc9838d58bd0dfba
Sergey Demyanov <s.demyanov@gmail.com>
2015-10-07 03:40:16 -0400

    fixes #12, #11

--- config/api.js
+++ config/api.js
@@ -1,18 +1,19 @@
-﻿var envexpand = function(str){
+﻿var envexpand = function (str) {
     if (!str) return str;
-	return str.replace(/\$([A-Za-z0-9_\-]+)/g, function(_,n) {
-    	return process.env[n];
+    return str.replace(/\$([A-Za-z0-9_\-]+)/g, function (_, n) {
+        return process.env[n];
     });
 };
 
 module.exports = {
-  
-  api_url: envexpand(process.env.DH_API_URL) || "http://nn8576.pg.devicehive.com/api",
-  admin_url: envexpand(process.env.DH_ADMIN_URL) || "http://nn8576.pg.devicehive.com/admin",
-  swagger_url: envexpand(process.env.DH_SWAGGER_URL) || "http://nn8576.pg.devicehive.com/api/swagger",
 
-  api: envexpand(process.env.DH_API) || "http://nn8576.pg.devicehive.com/api",    
-  admin_username: process.env.DH_USER || 'dhadmin',
-  admin_password: process.env.DH_PASS || 'dhadmin_#911'
+    api_url: envexpand(process.env.DH_API_URL) || "http://playground.devicehive.com/api/rest",
+    admin_url: envexpand(process.env.DH_ADMIN_URL) || "http://playground.devicehive.com/admin",
+    swagger_url: envexpand(process.env.DH_SWAGGER_URL) || "http://playground.devicehive.com/api/swagger",
+
+    api: envexpand(process.env.DH_API) || "http://playground.devicehive.com/api/rest",
+    api_key: process.env.DH_API_KEY || 'DFXFrZ8VQIkOYECScBbBwsYinj+o8IlaLsRQ81wO+l8',
+
+    secret: process.env.DH_SECRET || 'in god we trust'
 
 };
