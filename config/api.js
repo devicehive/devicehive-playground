8d8d43fefbbd6d59a681c2f8b17e8cad3fc3ec21
Sergey Demyanov <demon-xxi@users.noreply.github.com>
2015-10-19 15:09:09 -0400

    Update api.js

--- config/api.js
+++ config/api.js
@@ -9,6 +9,7 @@ module.exports = {
 
     api_url: envexpand(process.env.DH_API_URL) || "http://playground.devicehive.com/api/rest",
     admin_url: envexpand(process.env.DH_ADMIN_URL) || "http://playground.devicehive.com/admin",
+    dashboard_url: envexpand(process.env.DH_DASHBOARD_URL) || "http://playground.devicehive.com/dashboard",
     swagger_url: envexpand(process.env.DH_SWAGGER_URL) || "http://playground.devicehive.com/api/swagger",
 
     api: envexpand(process.env.DH_API) || "http://playground.devicehive.com/api/rest",
