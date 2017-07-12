daf3a6aecb4a9b42f1cc05b6e3d9c407491dd78b
KristinaChl <kchaldranyan@gmail.com>
2017-07-12 16:36:28 +0300

    comments removed

--- server.js
+++ server.js
@@ -93,13 +93,6 @@ app.get('/*', function(req, res){
     throw new NotFound;
 });
 
-// catch 404 and forward to error handler
-/*app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});*/
-
 // error handlers
 
 // development error handler
