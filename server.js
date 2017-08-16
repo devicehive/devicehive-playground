4163e73c2efedeb12fc27298fb60c08b623afcd6
KristinaChl <kchaldranyan@gmail.com>
2017-08-16 17:55:30 +0300

    GA added, error handler corrected

--- server.js
+++ server.js
@@ -94,35 +94,24 @@ app.get('/*', function(req, res){
     throw new NotFound;
 });
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        if(err instanceof NotFound){
-            res.render('404', { locals: {
-                title : '404 - Not Found',
-            }, status: 404 });
-        } else {
-            res.render('error', { locals: {
-                message: err.message,
-                error: {}
-            }, status: (err.status || 500) });
-        }
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
+// error handler
 app.use(function (err, req, res, next) {
     if(err instanceof NotFound){
-        res.render('404', { locals: {
-            title : '404 - Not Found'
-        }, status: 404 });
+        res.status(404);
+        res.render('404', {
+            title : '404 - Not Found',
+        });
     } else {
-        res.render('error', { locals: {
-            message: err.message,
-            error: {}
-        }, status: (err.status || 500) });
+        if (app.get('env') === 'development') {
+            res.status(err.status || 500);
+            res.render('error', {
+                message: err.message,
+                error: err
+            });
+        } else {
+            res.status(500);
+            res.send('Oops! Something went wrong on server!');
+        }
     }
 });
 
